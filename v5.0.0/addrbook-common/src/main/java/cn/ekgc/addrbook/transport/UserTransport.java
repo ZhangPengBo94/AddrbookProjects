ad8b40cd12295279fbae5a77f06a55ed11c456f5
31678 <a94wangcun328@163.com>
2021-04-20 17:08:16 +0800

    创建 addrbook-common 项目

--- v5.0.0/addrbook-common/src/main/java/cn/ekgc/addrbook/transport/UserTransport.java
+++ v5.0.0/addrbook-common/src/main/java/cn/ekgc/addrbook/transport/UserTransport.java
@@ -3,7 +3,7 @@ package cn.ekgc.addrbook.transport;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "addrbook-provider")
+@FeignClient(name = "addrbook-providers")
 @RequestMapping("/user/trans")
 public interface UserTransport {
 }
